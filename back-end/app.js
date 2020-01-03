e5c45c9170bb7d96b1a3d3048cedc08563357f30
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-02 18:43:07 -0600

    deleted documenation folder - backend folers made with files

---
+++ back-end/app.js
@@ -0,0 +1,40 @@
+// imports enviroment variable file
+require("dotenv").config();
+
+// import installed modules here
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const session = require("express-session");
+
+// import controllers here
+const usersController = require("./controllers/usersController.js");
+
+const loginRequired = require("./middleware/users.js");
+
+// imports the database connection module
+require("./db/db.js");
+
+const PORT = process.env.PORT;
+const API_PATH = "/api/v1/";
+const app = express();
+
+// setup middleware here
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: false,
+    resave: false
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cors({ origin: "localhost:3000" }));
+
+// setup controllers here
+app.use(API_PATH + "users", usersController);
+
+// runs the server
+app.listen(PORT, () => {
+  console.log("App running on PORT: ", PORT);
+});
---
+++ back-end/middleware/users.js
@@ -0,0 +1,16 @@
+// requires users to be logged in to access certain routes
+const loginRequired = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    res.json({
+      data: {},
+      status: {
+        code: 403,
+        message: "You are not logged in"
+      }
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = loginRequired;
