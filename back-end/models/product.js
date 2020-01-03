ce820a923b5e67ff845ba4991ae95ecff9600a7b
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-03 11:05:52 -0600

    created productSchema and model

--- back-end/models/product.js
+++ back-end/models/product.js
@@ -2,10 +2,39 @@ const mongoose = require("mongoose")
 
 // this schema represents a Products
 const productSchema = new mongoose.Schema({
-	user: {
-		
-	}	
+	postedBy: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+
+	// main product images
+	coverImage: String,
+
+	// secondary product images
+	images: [{
+		type: String,
+		required: false
+	}]
+	name: String,
+	description: String,
+	price: Number,
+
+	// TODO - add array of Review ObjectIds when the review schema exists
+
+	lastUpdated: {
+    type: Date,
+    default: Date.now
+	},
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 })
+
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
 
 
 
