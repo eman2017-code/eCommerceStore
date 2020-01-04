528d4bf9294606acb14b507038f2abd2755b184d
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-04 15:01:24 -0600

    created cartItemSchema

---
+++ back-end/models/cartItem.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+const cartItemSchema = new mongoose.Schema({
+	product: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Product'
+	}, 
+	quantity: {
+		type: Number,
+		default: 1
+	},
+	lastUpdated: {
+  	type: Date,
+    default: Date.now
+	},
+	timestamp: {
+	  type: Date,
+	  default: Date.now
+  }
+})
+
+const CartItem = mongoose.model('CartItem', cartItemSchema)
+
+module.exports = CartItem
+
