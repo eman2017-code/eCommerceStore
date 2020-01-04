a4bcdb01cc023054825e64ccabc2de35ac7dc766
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-04 14:50:47 -0600

    cart show route works

--- back-end/controllers/cartsController.js
+++ back-end/controllers/cartsController.js
@@ -5,6 +5,25 @@ const loginRequired = require('../middleware/users/loginRequired')
 const router = express.Router()
 
 
+// Show Route
+// this route shows a single cart
+router.get('/:cartId/', loginRequired, async (req, res, next) => {
+	try {
+		const foundCart = await Cart.findById(req.params.cartId)
+
+		res.json({
+			data: foundCart,
+			status: {
+				code: 200,
+				message: 'Successfully found cart'
+			}
+		}) 	
+	} catch (error) {
+		next(error);
+	}	
+})
+
+
 // Create Route 
 // this route is where a users cart is created
 router.post('/', loginRequired, async (req, res, next) => {
