94291b1992abbb3a41d777c950bf8c9692467121
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-04 14:43:10 -0600

    cart create route works

--- back-end/controllers/cartsController.js
+++ back-end/controllers/cartsController.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const Cart = require('../models/cart.js')
+const loginRequired = require('../middleware/users/loginRequired')
 
 const router = express.Router()
 
 
+// Create Route 
+// this route is where a users cart is created
+router.post('/', loginRequired, async (req, res, next) => {
+	try {
+		const newCart = await Cart.create({
+			user: req.session.userId
+		})
 
+		res.json({
+			data: newCart,
+			status: {
+				code: 201,
+				message: 'Successfully created cart'
+			}
+		})
+	} catch (error) {
+		next(error);
+	}
+})
 
 
 module.exports = router
