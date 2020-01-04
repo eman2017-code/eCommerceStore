f666300eac4dfdb8039dc998eb09dbac6936db18
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-04 15:25:36 -0600

    CartItem create route works, *doesnt get added to Cart

--- back-end/controllers/cartItemsController.js
+++ back-end/controllers/cartItemsController.js
@@ -5,7 +5,27 @@ const loginRequired = require('../middleware/users/loginRequired')
 const router = express.Router()
 
 
+// Create Route
+// this route create a new cart item and adds it to the users cart
+router.post('/', async (req, res, next) => {
+	const productId = req.body.productId
 
+	try {
+		const newCartItem = await CartItem.create({
+			'product': productId 
+		})
+
+		res.json({
+			data: newCartItem,
+			status: {
+				code: 201,
+				message: 'Product added to cart'
+			}
+		})
+	} catch (error) {
+		next(error);
+	}
+})
 
 
 module.exports = router
