f3a51117519454a2b368f5ddbedc3c3ec04d749d
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-06 00:22:43 -0600

    got rid of Breadcrumb in register component

--- front-end/src/components/pages/register.jsx
+++ front-end/src/components/pages/register.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 
-import Breadcrumb from "../common/breadcrumb";
-
 class Register extends Component {
   constructor(props) {
     super(props);
