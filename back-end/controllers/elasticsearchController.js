e4d183632622b79497bbe28778ceec495ed3205f
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-08 21:22:19 -0600

    beginning to set up filter route

--- back-end/controllers/elasticsearchController.js
+++ back-end/controllers/elasticsearchController.js
@@ -3,105 +3,189 @@ const router = express.Router();
 
 // require elasticsearch
 const { Client } = require("@elastic/elasticsearch");
+
 // connect to elasticsearch port
-const client = new Client({ node: "http://localhost:9200" });
+const client = new Client({ node: "http://35.224.98.206:9200/" });
+// console.log(client);
 
-// this function will mock-run all the elasticsearch queries
-async function run() {
-  // index the products.json file
-  await client.index({
-    index: "products",
-    body: {
-      category: "products"
-    }
-  });
+// handling errors
+const { errors } = require("@elastic/elasticsearch");
+// console.log(errors);
 
-  // search for category
-  const { body } = await client.search({
-    index: "products",
-    body: {
-      query: {
-        match: { category: "electronics" }
-      }
-    }
-  });
+// // index route
+// router.get("/products", function(req, res, next) {
+//   productListingPage(req.params.input).then(function(result) {
+//     res.json(result);
+//   });
+// });
 
-  // print out results
-  //   console.log(body.hits.hits);
-}
+// // load product listing page route
+// function productListingPage() {
+//   return client.indices.putMapping({
+//     index: "ecommerce-app",
+//     type: "document",
+//     body: {
+//       properties: {
+//         name: { type: "string" },
+//         suggest: {
+//           type: "completion",
+//           analyzer: "simple",
+//           search_analyzer: "simple",
+//           payloads: true
+//         }
+//       }
+//     }
+//   });
+// }
 
-// this function will mock-run all the elasticsearch queries
-async function run() {
-  // index the products.json file
-  await client.index({
-    index: "products",
-    body: {
-      category: "products"
-    }
-  });
+// exports.productListingPage = productListingPage;
+// console.log((exports.productListingPage = productListingPage));
 
-  // search for category
-  const { body } = await client.search({
-    index: "products",
-    body: {
-      query: {
-        match: { category: "fashion" }
-      }
-    }
-  });
+// // load product listing page route
+// async function productListingPage() {
+//   try {
+//     // calback API
+//     client.search(
+//       {
+//         index: "ecommerce-app",
+//         from: 20,
+//         size: 10,
+//         body: { foo: "ecommerce-app" }
+//       },
+//       {
+//         ignore: [404],
+//         maxRetries: 3
+//       },
+//       (err, result) => {
+//         if (err) {
+//           console.log("err");
+//           console.log(err);
+//         } else {
+//           console.log("results");
+//           console.log(result);
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     next(err);
+//   }
+// }
 
-  // print out results
-  //   console.log(body.hits.hits);
-}
+// console.log(productListingPage());
 
-// this function will mock-run all the elasticsearch queries
-async function run() {
-  // index the products.json file
-  await client.index({
-    index: "products",
-    body: {
-      category: "products"
-    }
-  });
+// // function to get index all of the products in the data.json file
+// async function run() {
+//   try {
+//     const clientIndex = await client.index({
+//       index: "products",
+//       body: {
+//         products: "products"
+//       }
+//     });
+//     console.log("clientIndex");
+//     console.log(clientIndex);
+//     console.log("body.hits.hits");
+//     console.log(body.hits.hits);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
 
-  // search for category
-  const { body } = await client.search({
-    index: "products",
-    body: {
-      query: {
-        match: { category: "family" }
-      }
-    }
-  });
+// // this function will mock-run all the elasticsearch queries
+// async function run() {
+//   // index the products.json file
+//   await client.index({
+//     index: "products",
+//     body: {
+//       category: "products"
+//     }
+//   });
+// }
 
-  // print out results
-  //   console.log(body.hits.hits);
-}
+//   // search for category
+//   const { body } = await client.search({
+//     index: "products",
+//     body: {
+//       query: {
+//         match: { category: "electronics" }
+//       }
+//     }
+//   });
 
-// this function will mock-run all the elasticsearch queries
-async function run() {
-  // index the products.json file
-  await client.index({
-    index: "products",
-    body: {
-      category: "products"
-    }
-  });
+//   // print out results
+//   //   console.log(body.hits.hits);
+// }
 
-  // search for category
-  const { body } = await client.search({
-    index: "products",
-    body: {
-      query: {
-        match: { category: "phones" }
-      }
-    }
-  });
+// // this function will mock-run all the elasticsearch queries
+// async function run() {
+//   // index the products.json file
+//   await client.index({
+//     index: "products",
+//     body: {
+//       category: "products"
+//     }
+//   });
 
-  // print out results
-  //   console.log(body.hits.hits);
-}
+//   // search for category
+//   const { body } = await client.search({
+//     index: "products",
+//     body: {
+//       query: {
+//         match: { category: "fashion" }
+//       }
+//     }
+//   });
 
-// run().catch(console.log);
+//   // print out results
+//   //   console.log(body.hits.hits);
+// }
+
+// // this function will mock-run all the elasticsearch queries
+// async function run() {
+//   // index the products.json file
+//   await client.index({
+//     index: "products",
+//     body: {
+//       category: "products"
+//     }
+//   });
+
+//   // search for category
+//   const { body } = await client.search({
+//     index: "products",
+//     body: {
+//       query: {
+//         match: { category: "family" }
+//       }
+//     }
+//   });
+
+//   // print out results
+//   //   console.log(body.hits.hits);
+// }
+
+// // this function will mock-run all the elasticsearch queries
+// async function run() {
+//   // index the products.json file
+//   await client.index({
+//     index: "products",
+//     body: {
+//       category: "products"
+//     }
+//   });
+
+//   // search for category
+//   const { body } = await client.search({
+//     index: "products",
+//     body: {
+//       query: {
+//         match: { category: "phones" }
+//       }
+//     }
+//   });
+
+//   // print out results
+//   //   console.log(body.hits.hits);
+// }
 
 module.exports = router;
