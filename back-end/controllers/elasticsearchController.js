ec4480899d9d527fd3b5321db44a495e370fff19
Mitchell Pottratz <mitchellpottratz@Mitchells-MacBook-Air.local>
2020-01-09 20:39:59 -0600

    created route to search for products

--- back-end/controllers/elasticsearchController.js
+++ back-end/controllers/elasticsearchController.js
@@ -74,6 +74,34 @@ router.get('/category/:categoryName/', async (req, res, next) => {
   }
 })
 
+// searches for products
+router.get('/products/', async (req, res, next) => {
+  const searchTerm = req.body.searchTerm
+
+  try {
+    const results = await client.search({
+      index: 'store-products-catalog2-cats',
+      body: {
+        query: {
+          match_phrase_prefix: {
+            'message.name': {
+              query: searchTerm,
+              slop: 2,
+              max_expansions: 10,
+            }
+          }
+        }
+      }
+    })
+
+    res.json({
+      data: results
+    })
+  } catch (error) {
+     next(error)
+  } 
+})
+
 
 
 module.exports = router;
