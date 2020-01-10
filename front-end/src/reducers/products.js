8bd5c9daa6b1a7773ccf5bcd7eb4467285865b57
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-10 11:32:32 -0600

    added break; to switch case

--- front-end/src/reducers/products.js
+++ front-end/src/reducers/products.js
@@ -1,34 +1,35 @@
 import {
-    FETCH_SINGLE_PRODUCT,
-    CHANGE_CURRENCY,
-    RECEIVE_PRODUCTS } from "../constants/ActionTypes";
-
+  FETCH_SINGLE_PRODUCT,
+  CHANGE_CURRENCY,
+  RECEIVE_PRODUCTS
+} from "../constants/ActionTypes";
 
 const initialState = {
-    products: [],
-    symbol: '$',
-    product_details: []
+  products: [],
+  symbol: "$",
+  product_details: []
 };
 
 const productReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case RECEIVE_PRODUCTS:
-            return { ...state,
-                products: action.products };
-        case FETCH_SINGLE_PRODUCT:
-            if (state.products.findIndex(product => product.id === action.productId) !== -1) {
-                const singleItem = state.products.reduce((itemAcc, product) => {
-                    return product
-                }, [])
-                return { ...state,
-                    product_details: singleItem };
-            }
+  switch (action.type) {
+    case RECEIVE_PRODUCTS:
+      return { ...state, products: action.products };
+    case FETCH_SINGLE_PRODUCT:
+      if (
+        state.products.findIndex(product => product.id === action.productId) !==
+        -1
+      ) {
+        const singleItem = state.products.reduce((itemAcc, product) => {
+          return product;
+        }, []);
+        return { ...state, product_details: singleItem };
+      }
+      break;
 
-        case CHANGE_CURRENCY:
-            return { ...state,
-                symbol: action.symbol };
-        default:
-            return state;
-    }
+    case CHANGE_CURRENCY:
+      return { ...state, symbol: action.symbol };
+    default:
+      return state;
+  }
 };
 export default productReducer;
