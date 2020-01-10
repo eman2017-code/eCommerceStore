c2df700a467d8db005403608c93e3cd7c60aea5b
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-09 23:38:51 -0600

    hooking up elasticsearch index route to be dispatched to store

--- front-end/src/api/shop.js
+++ front-end/src/api/shop.js
@@ -11,3 +11,21 @@ export default {
   buyProducts: (payload, cb, timeout) =>
     setTimeout(() => cb(), timeout || TIMEOUT)
 };
+
+// // global variable
+// let products;
+
+// // make fetch call to elastic index route
+// function listAllProducts() {
+//   const api = process.env.REACT_APP_API_URL + "/api/v1/search/all-products/";
+//   fetch(api)
+//     .then(reponse => reponse.json())
+//     .then(data => (products = data.data));
+//   // .then(data => console.log(data.data));
+//   console.log("products");
+//   console.log(products);
+// }
+
+// listAllProducts();
+// console.log("products");
+// console.log(products);
