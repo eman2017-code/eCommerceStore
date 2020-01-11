786b7842e426544af880b4015282159fc0116a98
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-11 13:10:21 -0600

    took away price filter for now

--- front-end/src/components/collection/common/filter.jsx
+++ front-end/src/components/collection/common/filter.jsx
@@ -53,7 +53,7 @@ class Filter extends Component {
         </div>
 
         {/* price filter */}
-        <SlideToggle>
+        {/* <SlideToggle>
           {({ onToggle, setCollapsibleElement }) => (
             <div className="collection-collapse-block open">
               <h3 className="collapse-block-title" onClick={onToggle}>
@@ -76,7 +76,7 @@ class Filter extends Component {
               </div>
             </div>
           )}
-        </SlideToggle>
+        </SlideToggle> */}
 
         {/* manufacturer filter here  */}
         <SlideToggle>
