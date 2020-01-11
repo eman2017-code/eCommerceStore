a87cfbffe83058210dd177319205b3be5ada0d3d
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-11 14:35:48 -0600

    getCartTotal function correctly calculates cart total

--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -104,9 +104,7 @@ export const getVisibleproducts = (
 export const getCartTotal = cartItems => {
   var total = 0;
   for (var i = 0; i < cartItems.length; i++) {
-    total +=
-      parseInt(cartItems[i].qty, 10) *
-      parseInt((cartItems[i].price * cartItems[i].discount) / 100, 10);
+    total += cartItems[i].qty * cartItems[i].price;
   }
   return total;
 };
