9616911025cef312138acf52a558853682b43648
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-13 13:40:27 -0600

    changed PageNotFound from class to function

--- front-end/src/components/pages/404.jsx
+++ front-end/src/components/pages/404.jsx
@@ -1,36 +1,28 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+import React from "react";
 
 import Breadcrumb from "../boilerplates/breadcrumb";
 
-class PageNotFound extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <Breadcrumb title={"404 Page"} />
-
-        <section className="p-0">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="error-section">
-                  <h1>404</h1>
-                  <h2>page not found</h2>
-                  <a href="/" className="btn btn-solid">
-                    back to home
-                  </a>
-                </div>
+function PageNotFound() {
+  return (
+    <div>
+      <Breadcrumb title={"404 Page"} />
+      <section className="p-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="error-section">
+                <h1>404</h1>
+                <h2>page not found</h2>
+                <a href="/" className="btn btn-solid">
+                  back to home
+                </a>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default PageNotFound;
