f88667a1fd1461f7de2bdae488c2b01a6b53ce36
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-13 11:34:54 -0600

    reverting back to original switch case for FETCH_SINGLE_PRODUCT

--- front-end/src/reducers/products.js
+++ front-end/src/reducers/products.js
@@ -63,15 +63,6 @@ const productReducer = (state = initialState, action) => {
         }, []);
         return { ...state, product_details: singleItem };
       }
-      // case FETCH_SINGLE_PRODUCT:
-      //   // itterate through the products array
-      //   const singleItem = state.data.products.filter(
-      //     product => product.sku === action.productId
-      //   );
-      //   // grab the first (and only one) that meets the requirements out of array
-      //   const matchingProduct = singleItem[0];
-      //   // put that item in the products_details array
-      //   return { ...state, product_details: matchingProduct };
       break;
 
     case CHANGE_CURRENCY:
