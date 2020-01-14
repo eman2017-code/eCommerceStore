a034e70ed35a05fbdb9d6e476a7b45a74940c17f
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-14 12:57:59 -0600

    user search results print to console

--- front-end/src/components/collection/common/product-listing.jsx
+++ front-end/src/components/collection/common/product-listing.jsx
@@ -6,14 +6,50 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { addToCart } from "../../../actions";
 import { getVisibleproducts } from "../../../services";
 import ProductListItem from "./product-list-item";
-import Search from "../../pages/search";
 
 class ProductListing extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { limit: 5, hasMoreItems: true };
+    this.state = { limit: 5, hasMoreItems: true, searchTerm: "", results: [] };
   }
+
+  // handle change
+  handleChange = e => {
+    e.preventDefault();
+    // handles value
+    this.setState({
+      searchTerm: e.target.value
+    });
+
+    this.queryProduct();
+  };
+
+  // route to search for products
+  queryProduct = async () => {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + "/api/v1/search/products/",
+        {
+          method: "POST",
+          body: JSON.stringify({ searchTerm: this.state.searchTerm }),
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      // convert response to json
+      const parsedResponse = await response.json();
+      console.log("parsedResponse");
+      console.log(parsedResponse);
+      // add results to state
+      this.setState({
+        results: [...parsedResponse.data]
+      });
+    } catch (err) {}
+  };
 
   componentWillMount() {
     this.fetchMoreItems();
@@ -34,9 +70,52 @@ class ProductListing extends Component {
 
   render() {
     const { products, addToCart, symbol } = this.props;
+    const { results } = this.state;
     return (
       <div>
-        <Search />
+        <div>
+          {/* Search section */}
+          <section className="authentication-page section-b-space">
+            <div className="container">
+              <section className="search-block">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-6 offset-lg-3">
+                      <form className="form-header">
+                        <div className="input-group">
+                          <input
+                            type="text"
+                            className="form-control"
+                            aria-label="Amount (to the nearest dollar)"
+                            placeholder="Search Products......"
+                            value={this.state.searchTerm}
+                            onChange={this.handleChange}
+                            autoComplete="on"
+                          />
+                          <div className="input-group-append">
+                            <button
+                              onClick={this.queryProduct}
+                              className="btn btn-solid"
+                            >
+                              <i className="fa fa-search"></i>Search
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </section>
+          <ul>
+            {results.map(result => (
+              <li key={result.name}>{result.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Listing products section */}
         <div className="product-wrapper-grid">
           <div className="container-fluid">
             {products.length > 0 ? (
