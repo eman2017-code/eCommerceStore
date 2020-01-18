09b4641d31c54b5f011104c5b010aba505194820
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-18 12:35:40 -0600

    changed mongo server to run off MongoDB Atlas vs localhost

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-
-
-const databaseConnectionString = "mongodb://localhost/ecommerce";
-// const databaseConnectionString =
-//   "mongodb+srv://admin:password123*@cluster0-bs1fx.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// const databaseConnectionString = "mongodb://localhost/ecommerce";
+const databaseConnectionString =
+  "mongodb+srv://admin:password123*@cluster0-bs1fx.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(databaseConnectionString, {
   useNewUrlParser: true,
