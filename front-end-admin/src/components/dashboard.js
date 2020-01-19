41a34f3551a472ade85d99542b06703d7d1b5e10
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-19 15:20:44 -0600

    made table scructure for how my_orders section will be

--- front-end-admin/src/components/dashboard.js
+++ front-end-admin/src/components/dashboard.js
@@ -386,41 +386,45 @@ export class Dashboard extends Component {
                               <thead>
                                 <tr>
                                   <th scope="col">Order ID</th>
-                                  <th scope="col">Order Total</th>
-                                  <th scope="col">Payment Method</th>
+                                  <th scope="col">Description</th>
+                                  <th scope="col">Total</th>
                                 </tr>
                               </thead>
                               <tbody>
                                 <tr>
                                   <td>1</td>
-                                  <td className="digits">$120.00</td>
-                                  <td className="font-danger">
-                                    Bank Transfers
+                                  <td className="digits">
+                                    product description goes here
                                   </td>
+                                  <td className="counter">$120.00</td>
                                 </tr>
                                 <tr>
                                   <td>2</td>
-                                  <td className="digits">$90.00</td>
-                                  <td className="font-secondary">Ewallets</td>
+                                  <td className="digits">
+                                    product description goes here
+                                  </td>
+                                  <td className="counter">$90.00</td>
                                 </tr>
                                 <tr>
                                   <td>3</td>
-                                  <td className="digits">$240.00</td>
-                                  <td className="font-warning">Cash</td>
+                                  <td className="digits">
+                                    product description goes here
+                                  </td>
+                                  <td className="counter">$240.00</td>
                                 </tr>
                                 <tr>
                                   <td>4</td>
-                                  <td className="digits">$120.00</td>
-                                  <td className="font-primary">
-                                    Direct Deposit
+                                  <td className="digits">
+                                    product description goes here
                                   </td>
+                                  <td className="counter">$120.00</td>
                                 </tr>
                                 <tr>
                                   <td>5</td>
-                                  <td className="digits">$50.00</td>
-                                  <td className="font-primary">
-                                    Bank Transfers
+                                  <td className="digits">
+                                    product description goes here
                                   </td>
+                                  <td className="counter">$50.00</td>
                                 </tr>
                               </tbody>
                             </table>
