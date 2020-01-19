dd857f4f53ff37d3faa973d66085e0b37b7d5289
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-19 15:08:47 -0600

    created ElasticSearchManager method to find products by category

--- back-end/managers/ElasticSearchManager.js
+++ back-end/managers/ElasticSearchManager.js
@@ -19,7 +19,7 @@ class ElasticSearchManager {
 		return client
 	}
 
-	// takes an elastic search response and returns just the search results
+	// takes an elastic search response as a parameter and returns just the search results
 	parseResponse(response) {
 		const results = response.body.hits.hits
 		return results
@@ -34,6 +34,24 @@ class ElasticSearchManager {
 	        body: {}
     	})
     	const products = this.parseResponse(response)
+    	return products
+	}
+
+	// return products based on whatever category is specified in the parameter
+	async getProductsByCategory(size, category) {
+		const response = await this.client.search({
+			index: "store-products-catalog2-cats",
+		    body: {
+	        	query: {
+	          		multi_match: {
+	            		query: category,
+	            		fields: ["message.category.name^2", "message.name"]
+	          		}
+	        	},
+	        	size: size
+      		}
+		})
+		const products = this.parseResponse(response)
     	return products
 	}
 
