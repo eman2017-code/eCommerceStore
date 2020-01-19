7f9f431c77db2d77cf1bfac5583a18eaebd5dd72
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-19 11:24:33 -0600

    got rid of heart icon in footer

--- front-end-admin/src/components/common/footer.js
+++ front-end-admin/src/components/common/footer.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <footer className="footer">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-md-6 footer-copyright">
-                                <p className="mb-0">Copyright 2019 © Multikart All rights reserved.</p>
-                            </div>
-                            <div className="col-md-6">
-                                <p className="pull-right mb-0">Hand crafted & made with<i className="fa fa-heart"></i></p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6 footer-copyright">
+                <p className="mb-0">
+                  Copyright 2019 © E-Commerce Store All rights reserved.
+                </p>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
