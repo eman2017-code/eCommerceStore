07699667dc6b4bb5cbab81f292f809745a4b0160
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-19 09:11:54 -0600

    removed id in listUser.js

--- front-end-admin/src/assets/data/listUser.js
+++ front-end-admin/src/assets/data/listUser.js
@@ -8,121 +8,101 @@ import user from "../images/dashboard/user.png";
 
 export const data = [
   {
-    id: "1",
     First_Name: "Rowan",
     Last_Name: "Torres",
     email: "Rowan.torres@gmail.com"
   },
   {
-    id: "2",
     First_Name: "Alonzo",
     Last_Name: "Perez",
     email: "Perez.Alonzo@gmail.com"
   },
   {
-    id: "3",
     First_Name: "Skylar",
     Last_Name: "Lane",
     email: "Lane.Skylar@gmail.com"
   },
   {
-    id: "4",
     First_Name: "Brody",
     Last_Name: "Gray",
     email: "Gray.Brody@gmail.com"
   },
   {
-    id: "5",
     First_Name: "Colton",
     Last_Name: "Clay",
     email: "Colton.Clay@gmail.com"
   },
   {
-    id: "6",
     First_Name: "Maxine",
     Last_Name: "Woters",
     email: "woters.maxine@gmail.com"
   },
   {
-    id: "7",
     First_Name: "Alonzo",
     Last_Name: "Perez",
     email: "Perez.Alonzo@gmail.com"
   },
   {
-    id: "8",
     First_Name: "Skylar",
     Last_Name: "Lane",
     email: "Lane.Skylar@gmail.com"
   },
   {
-    id: "9",
     First_Name: "Brody",
     Last_Name: "Gray",
     email: "Gray.Brody@gmail.com"
   },
   {
-    id: "10",
     First_Name: "Colton",
     Last_Name: "Clay",
     email: "Colton.Clay@gmail.com"
   },
   {
-    id: "11",
     First_Name: "Maxine",
     Last_Name: "Woters",
     email: "woters.maxine@gmail.com"
   },
   {
-    id: "12",
     First_Name: "Alonzo",
     Last_Name: "Perez",
     email: "Perez.Alonzo@gmail.com"
   },
   {
-    id: "13",
     First_Name: "Skylar",
     Last_Name: "Lane",
     email: "Lane.Skylar@gmail.com"
   },
   {
-    id: "14",
     First_Name: "Brody",
     Last_Name: "Gray",
     email: "Gray.Brody@gmail.com"
   },
   {
-    id: "15",
     First_Name: "Colton",
     Last_Name: "Clay",
     email: "Colton.Clay@gmail.com"
   },
   {
-    id: "16",
     First_Name: "Rowan",
     Last_Name: "Torres",
     email: "Rowan.torres@gmail.com"
   },
   {
-    id: "17",
     First_Name: "Alonzo",
     Last_Name: "Perez",
     email: "Perez.Alonzo@gmail.com"
   },
   {
-    id: "18",
     First_Name: "Skylar",
     Last_Name: "Lane",
     email: "Lane.Skylar@gmail.com"
   },
   {
-    id: "19",
     First_Name: "Brody",
     Last_Name: "Gray",
     email: "Gray.Brody@gmail.com"
   },
   {
-    id: "20",
     First_Name: "Colton",
     Last_Name: "Clay",
     email: "Colton.Clay@gmail.com"
