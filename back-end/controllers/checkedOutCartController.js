b745a8997a3827d004c29b8f76681019e953d66c
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-27 16:20:20 -0600

    checkout post route checks out guest users

--- back-end/controllers/checkedOutCartController.js
+++ back-end/controllers/checkedOutCartController.js
@@ -3,6 +3,7 @@ const CheckedOutCart = require('../models/checkedOutCart.js');
 const Cart = require('../models/cart.js');
 const CartItem = require('../models/cartItem.js');
 const Product = require('../models/product.js');
+const ElasticSearchManager = require("../managers/ElasticSearchManager.js");
 
 const router = express.Router();
 
@@ -27,19 +28,15 @@ router.post('/', async (req, res, next) => {
 
             // gets all the products that are in the cart that was checked out
             const productsToAdd = foundCart.cartItems.map(cartItem => cartItem.product._id);
-            console.log('productToAdd:', productsToAdd);
 
-            // creating this document officially 'checks out' the user
             const newCheckedOutCart = await CheckedOutCart.create({
                 user: userId,
                 products: [...productsToAdd]
             })
-            console.log('newCheckedOutCart:', newCheckedOutCart);
 
             // removes the cart items from the users cart since they just checked out
             foundCart.cartItems = [];
             await foundCart.save()
-            console.log('foundCart after deleting cart items:', foundCart);
 
             res.json({
                 data: newCheckedOutCart,
@@ -51,7 +48,40 @@ router.post('/', async (req, res, next) => {
 
         // if the user that checked out is a guest
         } else {
+            const productsToAdd = clientData.products;
 
+            // iterates through each product in the guest users cart
+            productsToAdd.forEach(async (productId) => {
+                let foundProduct = await Product.findOne({ 'upc': productId });
+
+                // if the product does not exists in mongodb
+                if (foundProduct === null) {
+
+                    // gets the product from elasticsearch
+                    const elasticSearchManager = new ElasticSearchManager();
+                    const productData = await elasticSearchManager.getProductByUPC(productId);
+                    productData.category = undefined;
+
+                    // create the product so now its in mongodb
+                    foundProduct = await Product.create(productData);
+                }
+            })
+
+            const newCheckedOutCart = await CheckedOutCart.create({
+                isGuess: true,
+                guestFirstName: clientData.firstName,
+                guestLastName: clientData.lastName,
+                guestEmail: clientData.email,
+                products: [...clientData.products]
+            })
+
+            res.json({
+                data: newCheckedOutCart,
+                status: {
+                    code: 201,
+                    message: 'Successfully checked out'
+                }
+            })
         }
 
     } catch (error) {
