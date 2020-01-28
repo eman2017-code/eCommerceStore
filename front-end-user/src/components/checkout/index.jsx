f53b1307af6cbd509339278fc4a02cb1c0dcfef5
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-27 18:48:32 -0600

    removed the form on the checkout page

--- front-end-user/src/components/checkout/index.jsx
+++ front-end-user/src/components/checkout/index.jsx
@@ -56,9 +56,9 @@ class checkOut extends Component {
 
         const client = {
             sandbox:
-              process.env.PAYPAL_SECRET_KEY,
+              "AdpXajVLZK--uQ1DtiDlLQSBFbc5-Sjy95D70RN8Uj1akkCgu-7QmdATnt1LdkgeaDGAeYhTVQyFhfsr",
             production:
-              process.env.PAYPAL_SECRET_KEY
+              "AdpXajVLZK--uQ1DtiDlLQSBFbc5-Sjy95D70RN8Uj1akkCgu-7QmdATnt1LdkgeaDGAeYhTVQyFhfsr"
           };
 
         return (
@@ -76,114 +76,23 @@ class checkOut extends Component {
                             <div className="checkout-form">
                                 <form>
                                     <div className="checkout row">
-                                        <div className="col-lg-6 col-sm-12 col-xs-12">
-                                            <div className="checkout-title">
-                                                <h3>Billing Details</h3>
-                                            </div>
+                                        
 
-                                            <div className="row check-out">
-                                                <div className="form-group col-md-6 col-sm-6 col-xs-12">
-                                                    <div className="field-label">First Name</div>
-                                                    <input
-                                                        type="text"
-                                                        name="first_name"
-                                                        value={this.state.first_name}
-                                                        onChange={this.handleInputChange}
-                                                    />
-                                                </div>
-                                                <div className="form-group col-md-6 col-sm-6 col-xs-12">
-                                                    <div className="field-label">Last Name</div>
-                                                    <input
-                                                        type="text"
-                                                        name="last_name"
-                                                        value={this.state.last_name}
-                                                        onChange={this.handleInputChange}
-                                                    />
-                                                </div>
-                                                <div className="form-group col-md-6 col-sm-6 col-xs-12">
-                                                    <div className="field-label">Phone</div>
-                                                    <input
-                                                        type="text"
-                                                        name="phone"
-                                                        value={this.state.phone}
-                                                        onChange={this.handleInputChange}
-                                                    />
-                                                </div>
-                                                <div className="form-group col-md-6 col-sm-6 col-xs-12">
-                                                    <div className="field-label">Email</div>
-                                                    <input
-                                                        type="email"
-                                                        name="email"
-                                                        value={this.state.email}
-                                                        onChange={this.handleInputChange}
-                                                    />
-                                                </div>
-                                                <div className="form-group col-md-12 col-sm-12 col-xs-12">
-                                                    <div className="field-label">Country</div>
-                                                    <select
-                                                        name="country"
-                                                        value={this.state.country}
-                                                        onChange={this.handleInputChange}
-                                                    >
-                                                        <option>India</option>
-                                                        <option>South Africa</option>
-                                                        <option>United States</option>
-                                                        <option>Australia</option>
-                                                    </select>    
-                                                </div>
-                                                <div className="form-group col-md-12 col-sm-12 col-xs-12">
-                                                    <div className="field-label">Address</div>
-                                                    <input
-                                                        type="text"
-                                                        name="address"
-                                                        value={this.state.address}
-                                                        onChange={this.handleInputChange}
-                                                    />   
-                                                </div>
-                                                <div className="form-group col-md-12 col-sm-12 col-xs-12">
-                                                    <div className="field-label">Town/City</div>
-                                                    <input
-                                                        type="text"
-                                                        name="city"
-                                                        value={this.state.city}
-                                                        onChange={this.handleInputChange}
-                                                    />   
-                                                </div>
-                                                <div className="form-group col-md-12 col-sm-6 col-xs-12">
-                                                    <div className="field-label">State</div>
-                                                    <input
-                                                        type="text"
-                                                        name="state"
-                                                        value={this.state.state}
-                                                        onChange={this.handleInputChange}
-                                                    />    
-                                                </div>
-                                                <div className="form-group col-md-12 col-sm-6 col-xs-12">
-                                                    <div className="field-label">Postal Code</div>
-                                                    <input
-                                                        type="text"
-                                                        name="pincode"
-                                                        value={this.state.spincode}
-                                                        onChange={this.handleInputChange}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-6 col-sm-12 col-xs-12">
+                                        <div className="col-lg-12 col-sm-12 col-xs-12">
                                             <div className="checkout-details">
                                                 <div className="order-box">
                                                     <div className="title-box">
-                                                        <div> Product <span> Total</span></div>
+                                                        <div>Products</div>
 
                                                         <ul className="qty">
                                                         {cartItems.map((item, index) => {
                                                             return (
-                                                                <li key={index}>
-                                                                    {item.name} × {item.qty}{" "}
-                                                                    <span>
+                                                                <li key={index} className="d-flex">
+                                                                    <p>{item.name} × {item.qty}{" "}</p>
+                                                                    <p className="px-2">-</p>
+                                                                    <p>
                                                                         {symbol} {total}
-                                                                    </span>
+                                                                    </p>
                                                                 </li>
                                                             );
                                                         })}
@@ -191,12 +100,6 @@ class checkOut extends Component {
                                                     </div>
 
                                                     <div className="d-flex justify-content-between">
-                                                        
-                                                        <button
-                                                            type="button"
-                                                            className="btn-solid btn">
-                                                            Pay With Card
-                                                        </button>
                                                         <PaypalExpressBtn
                                                             env={"sandbox"}
                                                             client={client}
@@ -206,6 +109,11 @@ class checkOut extends Component {
                                                             onSuccess={this.paypalSuccess}
                                                             onCancel={this.paypalCancel}
                                                         />
+                                                        <button
+                                                            type="button"
+                                                            className="btn-solid btn">
+                                                            Pay With Card
+                                                        </button>
                                                     </div>
                                                     
                                                     
