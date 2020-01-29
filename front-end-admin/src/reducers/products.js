8455f1e803dab7101c1e637eaa63d4cf2c70a052
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-28 18:07:48 -0600

    created a reducer for creating products

--- front-end-admin/src/reducers/products.js
+++ front-end-admin/src/reducers/products.js
@@ -1,6 +1,7 @@
 import {
   LIST_PRODUCTS_ADMIN,
-  RECEIVE_PRODUCTS
+  RECEIVE_PRODUCTS,
+  CREATE_PRODUCT
 } from "../constants/ActionTypes";
 
 const initialState = {
@@ -10,8 +11,17 @@ const initialState = {
 
 const productsReducer = (state = initialState, action) => {
   switch (action.type) {
+
     case RECEIVE_PRODUCTS:
       return { ...state, products: action.products };
+
+    case CREATE_PRODUCT:
+      state.products.push(action.product);
+      return {
+        ...state, 
+        products: state.products
+      }   
+
     default:
       return state;
   }
