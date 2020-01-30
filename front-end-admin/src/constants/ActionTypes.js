fcafc3ab6905ac574cea5edc0ffd990042009aa8
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-30 16:54:04 -0600

    create UPDATE_PRODUCT constant

--- front-end-admin/src/constants/ActionTypes.js
+++ front-end-admin/src/constants/ActionTypes.js
@@ -6,6 +6,7 @@ export const LOGOUT = "LOGOUT";
 // Products
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 
 
