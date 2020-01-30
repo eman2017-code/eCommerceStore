812d3a0202228fdfbbbdb815c292a2b2bd9a2b32
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-01-30 14:01:24 -0600

    created a reducer to delete an admins product

--- front-end-admin/src/constants/ActionTypes.js
+++ front-end-admin/src/constants/ActionTypes.js
@@ -7,3 +7,6 @@ export const LOGOUT = "LOGOUT";
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+
+
+
--- front-end-admin/src/reducers/products.js
+++ front-end-admin/src/reducers/products.js
@@ -1,4 +1,8 @@
-import { GET_PRODUCTS, CREATE_PRODUCT } from "../constants/ActionTypes";
+import { 
+  GET_PRODUCTS,
+  CREATE_PRODUCT,
+  DELETE_PRODUCT }
+from "../constants/ActionTypes";
 
 const initialState = {
   products: [],
@@ -21,7 +25,16 @@ const productsReducer = (state = initialState, action) => {
       return {
         ...state,
         products: state.products
-      };
+      }
+
+    // removes one of the admins products from the state
+    case DELETE_PRODUCT:
+      const updatedProducts = state.products.filter(product => product.upc != action.productId);
+
+      return {
+        ...state,
+        product: updatedProducts
+      }
 
     default:
       return state;
