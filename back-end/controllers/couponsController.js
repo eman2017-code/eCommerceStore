2828201c0ceb0bae70d49ed406fb7a8b50da73bc
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-01-31 14:04:50 -0600

    created index route to list all couponds

--- back-end/controllers/couponsController.js
+++ back-end/controllers/couponsController.js
@@ -3,6 +3,18 @@ const Coupons = require("../models/coupons.js");
 const loginRequired = require("../middleware/users/loginRequired.js");
 const router = express.Router();
 
-// create coupons route
+// index route
+router.get("/", async (req, res, next) => {
+  try {
+    const allCoupons = await Coupons.find({});
+    res.json({
+      data: allCoupons,
+      status: {
+        code: 200,
+        message: "successfully listed all coupons"
+      }
+    });
+  } catch (err) {}
+});
 
 module.exports = router;
